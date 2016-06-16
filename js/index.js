9be94637d36cb546a5ccdc561026d86a8c62e2c7
genestd <drwgenest@gmail.com>
2016-06-16 09:21:30 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+$("#myCarousel").carousel("pause");
 
+/*scrollspy behavior -
+ - add/remove hide class on brand when scrolling
+*/
+
+      // The event is fired when an item gets actived with the scrollspy
+      $("#myNavbar").on('activate.bs.scrollspy', function () {
+         var currentSection = $(".nav li.active > a").text();
+         if (currentSection == 'Home'){
+           $("#brand").removeClass("hide");
+
+        } else {
+           $("#brand").addClass('hide');
+          
+         }
+
+      });
